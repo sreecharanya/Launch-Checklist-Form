d34b1c5c025ad0549fe0f1f0e4756bfca5031d2c
sreecharanya <sree3charanya@gmail.com>
2020-07-27 18:58:09 -0500

    changed

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
 // Write your JavaScript code here!
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+window.addEventListener("load", function() {
+   let form = document.querySelector("Form");
+form.addEventListener("submit",function(event)
+{  
+      let pilotnameInput = document.querySelector("input[name=pilotName]");
+      let copilotnameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.queryaSelector("input[name=cargoMass]");
+      let faultyItems = document.getElementById("faultyItems");
+     // event.preventDefault();
+       if (pilotnameInput.value === "" || copilotnameInput.value === "" || fuelLevelInput.value ==="" || cargoMassInput.value==="")
+       {
+          console.log("hi");
+                    alert("All fields are required!");
+          
+         }else if(isNaN(pilotnameInput.value) === false ||  isNaN(copilotnameInput.value)=== false  || isNaN(fuelLevelInput.value)=== true || isNaN(cargoMassInput.value) === true) 
+         {        
+         alert("Invalid entry");
+          } else
+          {
+            
+            faultyItems.style.visibility = "visible"; 
+          
+          
+           if((fuelLevelInput.value) < 10000)
+           {
+            pilotStatus.innerHTML =`Pilot ${pilotnameInput.value} is ready for launch`;
+            copilotStatus.innerHTML=`Copilot ${copilotnameInput.value}is ready for launch`;
+            fuelStatus.innerHTML="Fuel too low for launch";
+            launchStatus.innerHTML="Shuttle not ready for launch";
+            launchStatus.style.color="red";
+           }
+         }  
+         
+      // stop the form submission
+        
+       });
+});
+/*This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
    <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
+<li>Diameter: ${}</li>
+   <li>Star: ${Tatoo I & Tatoo II}</li>
+   <li>Distance from Earth: ${43000 light years from galactic core}</li>
+   <li>Number of Moons: ${https://www.nasa.gov/sites/default/files/images/587837main_Kepler16_transit_art2_full.jpg}</li>
 </ol>
 <img src="${}">
 */
